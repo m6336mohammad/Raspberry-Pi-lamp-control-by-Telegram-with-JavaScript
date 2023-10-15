211cf210912a0463db5643be272cd3cea1207f54
mohammad <m6336.programer@gmail.com>
2023-10-15 12:30:33 +0330

    added bot.on and if (condition , switch )

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/ , msg=>{
 
-  bot.sendMessage(msg.chat.id, 'salam ' + '  ' + msg.chat.first_name + '    username:   ' + msg.chat.username +  '   your ID: ' + msg.chat.id,
+  bot.sendMessage(msg.chat.id, 'hello ' + '  ' + msg.chat.first_name + '    username:   ' + msg.chat.username +  '   your ID: ' + msg.chat.id,
   
     
       {
@@ -29,6 +29,45 @@ bot.onText(/\/start/ , msg=>{
     )
   
   })
+
+
+  bot.on('message' ,msg=>{
+    console.log(msg.text)
+    if(msg.text == 'on'){
+      bot.sendMessage(msg.chat.id, 'LED is ON banzen ')
+      led.turnOn()
+      
+      }else if(msg.text == 'off'){
+        
+      bot.sendMessage(msg.chat.id, 'LED is OFF banzen ')
+      turnOff()
+        
+        }
+        
+        
+        switch(msg.text){
+          
+          case 'ON':
+            bot.sendMessage(msg.chat.id, 'LED is ON banzen ')
+            led.turnOn()
+            break;
+          case 'OFF':
+            bot.sendMessage(msg.chat.id, 'LED is OFF banzen ')
+            turnOff()
+          break;
+          
+          case 'Abute':
+            bot.sendMessage(msg.chat.id, 'ba ba banzen K  ')
+          break;
+          }
+        
+    })
+
+
+
+
+
+
 
 //class for control pin out and on / off function
   class LEDController {
